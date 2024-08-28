9786d49c814a839696e8bc5e5b822711db751ae9
mumer17865 <116781030+mumer17865@users.noreply.github.com>
2024-08-28 11:02:48 +0500

    Update order-details.component.ts

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -26,7 +26,7 @@ export class OrderDetailsComponent implements OnInit {
   }
 
   fetchOrderDetails(id: number, orderId: number) {
-    axios.get(`http://localhost:3000/getHistory/history/${id}/details${orderId}`)
+    axios.get(`https://ecommerce-application-beryl.vercel.app/getHistory/history/${id}/details${orderId}`)
       .then((response) => {
         this.items = response.data;
       })
