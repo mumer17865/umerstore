a9c7358f150b0bd4603c100b5269cceb6d70bedc
mumer17865 <116781030+mumer17865@users.noreply.github.com>
2024-08-28 11:05:10 +0500

    Update productdetails.component.ts

--- src/app/productdetails/productdetails.component.ts
+++ src/app/productdetails/productdetails.component.ts
@@ -57,7 +57,7 @@ export class ProductdetailsComponent implements OnInit {
   }
 
   fetchProductDetails(productId: number) {
-    axios.get(`http://localhost:3000/products/itemList/${productId}`)
+    axios.get(`https://ecommerce-application-beryl.vercel.app/products/itemList/${productId}`)
       .then((response) => {
         this.Item = response.data[0];
       })
